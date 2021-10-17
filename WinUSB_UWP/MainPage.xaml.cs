f9598af9f0bf7c54671d3224fdb3416d3cbf85fb
VIVES-ELICT <hans.naert@vives.be>
2021-10-17 16:27:57 +0200

    add bulkread/write

--- WinUSB_UWP/MainPage.xaml.cs
+++ WinUSB_UWP/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Devices.Usb;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.Storage.Streams;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -30,7 +31,7 @@ namespace WinUSB_UWP
 
         UsbDevice usbDevice;
 
-        private async void connectButtonClick(object sender, RoutedEventArgs e)
+        private async void connectDevice()
         {
             UInt32 vid = 0xC251;
             UInt32 pid = 0x4307;
@@ -49,9 +50,83 @@ namespace WinUSB_UWP
             }
             finally
             {
-                USBFound.Text = "Opened device for communication.";
+                USBFound.Text = "Opened device for communication."+ ((usbDevice!=null)?myDevices[0].Name: "Warning usbDevice is Null");
             }
+        }
 
+        private void connectButtonClick(object sender, RoutedEventArgs e)
+        {
+            connectDevice();
+        }
+
+        private async void BulkWrite(byte dataByte)
+        {
+            UInt32 bytesWritten = 0;
+
+            UsbBulkOutPipe writePipe = usbDevice.DefaultInterface.BulkOutPipes[0];
+            writePipe.WriteOptions |= UsbWriteOptions.ShortPacketTerminate;
+
+            var stream = writePipe.OutputStream;
+
+            DataWriter writer = new DataWriter(stream);
+
+            writer.WriteByte(dataByte);
+
+            try
+            {
+                bytesWritten = await writer.StoreAsync();
+            }
+            catch (Exception exception)
+            {
+                USBFound.Text = exception.Message.ToString();
+            }
+            finally
+            {
+                //MessageBox.Show("Data written: " + bytesWritten + " bytes.");
+            }
+        }
+
+
+        private async void BulkRead()
+        {
+            UInt32 bytesRead = 0;
+
+            UsbBulkInPipe readPipe = usbDevice.DefaultInterface.BulkInPipes[0];
+            readPipe.ReadOptions |= UsbReadOptions.IgnoreShortPacket;
+
+            var stream = readPipe.InputStream;
+            DataReader reader = new DataReader(stream);
+
+            try
+            {
+                bytesRead = await reader.LoadAsync(readPipe.EndpointDescriptor.MaxPacketSize);
+            }
+            catch (Exception exception)
+            {
+                USBFound.Text = exception.Message.ToString();
+            }
+            finally
+            {
+                //MessageBox.Show("Number of bytes: " + bytesRead);
+
+                IBuffer buffer = reader.ReadBuffer(bytesRead);
+
+                var dataReader = DataReader.FromBuffer(buffer);
+                byte[] bytes = new byte[64];
+                dataReader.ReadBytes(bytes);
+
+                //potProgressBar.Value = bytes[1] == 1 ? 0 : 1023;
+
+                //this.BeginInvoke(new Action(() => label1.Text = bytes[1] == 1 ? "not pressed" : "pressed"));
+                USBFound.Text=buffer.ToString();
+
+            }
+        }
+
+        private void transferButtonClick(object sender, RoutedEventArgs e)
+        {
+            BulkWrite(0x81);
+            BulkRead();
         }
     }
 }
